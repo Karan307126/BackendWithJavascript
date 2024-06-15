d298bfa9dc86831f5e41c7ee2ad2d03bcde7bdf0
Karan Sarvaiya <sarvaiyakaran2003@gmail.com>
2024-06-16 00:13:46 +0530

    Add routes for change-password, current-user, update-account, update-avatar, update-cover-image, watch-history, channel profile

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   loginUser,
   registerUser,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getUserWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJwt } from "../middlewares/auth.middlewares.js";
@@ -23,5 +30,19 @@ router.route("/login").post(loginUser);
 // Secure Routes
 router.route("/logout").post(verifyJwt, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+
+router.route("/update-account").patch(verifyJwt, updateAccountDetails);
+
+router
+  .route("/update-avatar")
+  .patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/update-cover-image")
+  .patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/channel/:userName").get(verifyJwt, getUserChannelProfile);
+router.route("/watch-history").get(verifyJwt, getUserWatchHistory);
 
 export default router;
