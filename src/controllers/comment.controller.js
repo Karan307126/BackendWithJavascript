77c2ccff4d28dc60b41d16ad11075b927cad7f2f
Karan Sarvaiya <sarvaiyakaran2003@gmail.com>
2024-06-25 22:51:53 +0530

    Add addComment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -69,7 +69,43 @@ const getVideoComment = asyncHandler(async (req, res) => {
 });
 
 const addComment = asyncHandler(async (req, res) => {
-  // TODO: add a comment to a video
+  /*
+   1. Get content and userId from request body
+   2. Get videoId from request params
+   3. Find the user details using userId
+   4. Check if user exists, if not, throw an error
+   5. Create a new comment with videoId, userId, and content
+   6. Save the new comment to the database
+   7. Send back a response with the created comment details
+  */
+
+  const { videoId } = req.params;
+  const { content } = req.body;
+
+  const user = req.user;
+
+  if (!user) throw new ApiError(404, "User not found");
+
+  const comment = new Comment({
+    video: videoId,
+    owner: user._id,
+    content: content,
+  });
+
+  await comment.save();
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        content: comment.content,
+        userName: user.userName,
+        createdAt: comment.createdAt,
+        updatedAt: comment.updatedAt,
+      },
+      "Comment created successfully"
+    )
+  );
 });
 
 const updateComment = asyncHandler(async (req, res) => {
